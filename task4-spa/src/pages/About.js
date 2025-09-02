5e02b54c5f332bd8f6c05177683e2eb154c8f15b
Danz-ed <129077123+Danz-ed@users.noreply.github.com>
2025-09-02 12:48:53 +0000

    Solo commit

---
+++ task4-spa/src/pages/About.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function About() {
+  return <h2>About Us - This is a simple SPA using React.</h2>;
+}
+
+export default About;
---
+++ task4-spa/src/pages/Contact.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Contact() {
+  return <h2>Contact Page - Reach us at support@codveda.com</h2>;
+}
+
+export default Contact;
